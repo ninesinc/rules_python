630f687e075ce5dd2d069b343431dd7db7774b51
Andrew Ring <andrew.ring@ninesai.com>
2019-08-13 13:08:47 -0700

    Patch in workaround to allow google cloud pip package to be used.
    Issue documenting problem:
    https://github.com/bazelbuild/rules_python/issues/14
    
    Workaround patch source:
    https://github.com/AppliedIntuition/rules_python/commit/77f6258cd7ea027c281bf774305b8515800a5eb9

--- rules_python/whl.py
+++ rules_python/whl.py
@@ -105,6 +105,36 @@ class Wheel(object):
     with zipfile.ZipFile(self.path(), 'r') as whl:
       whl.extractall(directory)
 
+      names = set(whl.namelist())
+
+    # Workaround for https://github.com/bazelbuild/rules_python/issues/14
+    for initpy in self.get_init_paths(names):
+      with open(os.path.join(directory, initpy), 'w') as f:
+        f.write(INITPY_CONTENTS)
+
+   def get_init_paths(self, names):
+    # Overwrite __init__.py in these directories.
+    # (required as googleapis-common-protos has an empty __init__.py, which
+    # blocks google.api.core from google-cloud-core)
+    NAMESPACES = ["google/api"]
+
+     # Find package directories without __init__.py, or where the __init__.py
+    # must be overwritten to create a working namespace. This is based on
+    # Bazel's PythonUtils.getInitPyFiles().
+    init_paths = set()
+    for n in names:
+      if os.path.splitext(n)[1] not in ['.so', '.py', '.pyc']:
+        continue
+      while os.path.sep in n:
+        n = os.path.dirname(n)
+        initpy = os.path.join(n, '__init__.py')
+        initpyc = os.path.join(n, '__init__.pyc')
+        if (initpy in names or initpyc in names) and n not in NAMESPACES:
+          continue
+        init_paths.add(initpy)
+
+     return init_paths
+
   # _parse_metadata parses METADATA files according to https://www.python.org/dev/peps/pep-0314/
   def _parse_metadata(self, content):
     # TODO: handle fields other than just name
